057498daef7fb912f988f9229869d23b1d035005
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2022-05-07 14:55:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('Nm Cinema Group, url='https://t.me/NmCinemaGrp')
+            InlineKeyboardButton('🎬 Nm Cinema Group', url='https://t.me/NmCinemaGrp')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
